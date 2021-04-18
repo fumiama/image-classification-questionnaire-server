4aacceab897e55f0b570e7c90f8d7acc76d0cb86
fumiama <s862105088@gmail.com>
2021-04-18 19:11:56 +0800

    添加批量上传脚本

---
+++ post_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os, sys, stat
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        parent_dir = sys.argv[1]
+        if parent_dir[-1] != '/': parent_dir += '/'
+        files = os.listdir(parent_dir)
+        post_url = sys.argv[2]
+        #print(files)
+        with open("./post_all.sh", "w") as f:
+            f.write("#!/usr/bin/env bash\n")
+            for file_name in files:
+                if len(file_name) >= 5 and file_name[-5:] == ".webp":
+                    f.write("wget -qO- --post-file=" + parent_dir + file_name + ' ' + post_url + '\n')
+        os.chmod("./post_all.sh", stat.S_IRWXU)
+        os.system("./post_all.sh")
+    else: print("usage: <img_floder> <post_url>")
+
+        
