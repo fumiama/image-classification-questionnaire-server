171ee74d942e3f04daa478c241669b7e97073c41
fumiama <s862105088@gmail.com>
2021-08-19 19:44:18 +0800

    修正daemon

--- daemon.sh
+++ daemon.sh
@@ -2,9 +2,8 @@
 
 while :
 do
-    if [ $(pgrep -f "sudo ./server.py") ];then
-        sleep 5
-    else
+    if [ -z $(pgrep -f "sudo ./server.py") ];then
         sudo ./server.py -d ./users/ ./imgs/ ./pwd.txt 1000 2>&1 > ./log.txt &
     fi
+    sleep 10
 done
